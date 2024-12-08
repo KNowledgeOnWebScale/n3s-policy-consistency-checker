3d667af6eafa20331ffb5e3d5d84120a96c8b839
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2024-12-08 09:45:01 +0100

    Adding documentation

--- run.sh
+++ run.sh
@@ -7,5 +7,8 @@ if [ "$1" == "" ]; then
   exit 1
 fi
 
+# Compile to RDF Surfaces
 eye --nope --quiet compiler.n3 "$@" --query query.n3 > ${OUTPUT}
+
+# Run RDF Surfaces with deontic rules
 eye --nope --no-bnode-relabeling --quiet deontic.n3s ${OUTPUT}
