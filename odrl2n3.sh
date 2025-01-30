a190aef952b8d307c26f9573f1a956421543f3d4
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2025-01-30 11:40:10 +0100

    Adding support for data input

--- odrl2n3.sh
+++ odrl2n3.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+usage() { echo "Usage: $0 [-d <data-file>] odrl-file ['check']" 1>&2; exit 1; }
+
+while getopts "d:" o; do
+    case "${o}" in
+        d)
+            DATA=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 FILE=$1
 RUN=$2
 
 if [ "${FILE}" == "" ]; then
-    echo "usage: $0 file ['check'/]"
-    exit 1
+    usage
+fi
+
+data=$(mktemp)
+
+if [ "${DATA}" != "" ]; then
+    eye --nope --quiet --pass-only-new data2deo.n3 ${DATA} > ${data}
 fi
 
 if [ "${RUN}" == "check" ]; then
     temp=$(mktemp)
-    eye --nope --quiet ${FILE} odrl2deo.n3 --query odrl_query.n3 > ${temp}
+    eye --nope --quiet ${FILE} odrl2deo.n3 ${data} --query odrl_query.n3 > ${temp}
     ./run.sh ${temp}
     rm ${temp}
 else
-   eye --nope --quiet ${FILE} odrl2deo.n3 --query odrl_query.n3
+   eye --nope --quiet ${FILE} odrl2deo.n3 ${data} --query odrl_query.n3
 fi 
+
+rm ${data}
